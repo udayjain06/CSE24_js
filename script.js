ea36c561e0f626a97bb99d49c9869f12a31e861b
udayjain06 <jainuday51@gmail.com>
2026-02-24 18:36:58 +0530

    theory

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+// document.write("hello world")
+var num = 10;
+console.log(num);
+conslone.log(typeof(num));
+num=false;  
+console.log(typeof(null))
+null=null;
